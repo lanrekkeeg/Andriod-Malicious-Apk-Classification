f5d7ea9b6f529b3fcc3e42ac5215755a2703e1b6
Faisal <p156058@nu.edu.pk>
2018-03-18 19:32:36 +0500

    Add files via upload

---
+++ App_Permission_Extractor.py
@@ -0,0 +1,31 @@
+import re
+import os
+
+dir = "C:\\Users\\Faisal Khan\Desktop\\Machine-learning\\project\\Data-set\\drebin\\drebin-0\\drebin-0"
+change_to = "C:\\Users\\Faisal Khan\Desktop\\Machine-learning\\project\\Data-set\\drebin\\drebin-0\\drebin-0\\"
+
+root, dirs, files = os.walk(dir).__next__()
+out = open("feature.txt",'w')
+for folder in dirs:
+	new_Dir = change_to+folder
+	app_Hash_Key = folder.strip(".zip.out")
+	out.write(app_Hash_Key)
+	print(app_Hash_Key)
+	try:
+		file = open(new_Dir + "\\AndroidManifest.xml",'r',encoding="utf8") # utf8 because we had some chinese text in Manifest.XML
+	except (FileNotFoundError,IOError):
+		continue	
+	pattern = re.compile('<uses-permission')
+	line_list = []
+	for line in file:
+		return_True = pattern.search(line)
+		if return_True:
+			line_list.append(line)
+			
+	for line in line_list:
+		feature_Word = line.strip().split(".",)
+		feature_Word = feature_Word[len(feature_Word)-1].strip("\"/>")
+		out.write(" ")
+		out.write(feature_Word)
+	out.write('\n')
+		
