d195566124b8ec3a16139ea5b1b7e9778543d1cb
Faisal <p156058@nu.edu.pk>
2018-03-18 10:21:39 +0500

    Create debrin-apk-decoder.py

---
+++ debrin-apk-decoder.py
@@ -0,0 +1,11 @@
+import os
+import subprocess
+dir = "<your path to data-set folder>"
+cmd = "apktool d "
+root, dirs, files = os.walk(dir).__next__()
+print(files)
+for file in files:
+	temp = cmd
+	temp += file
+	subprocess.call(temp, shell=True) 
+	
